f7eb8f15df6e4225a506033c5f6bff509c411f34
imbiez <joshrispoli@gmail.com>
2013-07-12 14:53:03 -0500

    update

---
+++ emurun.sh
@@ -0,0 +1,9 @@
+agentloc='../sdk/mercury-2.2.2'
+emuloc='../sdk/sdk/tools'
+emuname='@Nexus'
+emuparams='-scale 0.5 &'
+emu_timer=0
+emu_timeout=25000
+
+eval './'$emuloc'/emulator '$emuname' '$emuparams
+
