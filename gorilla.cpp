3d779dcb36ee131ecd942c0ab31274f72a7022df
imbiez <joshrispoli@gmail.com>
2013-07-16 22:14:25 -0500

    refactored

--- gorilla.cpp
+++ gorilla.cpp
@@ -1,55 +1,32 @@
-/*
-	gorilla.cpp
-	Joshua Rispoli
-	7/9/2013
-	SUCCESS Lab
-
-	Fuzzes the Android platform using the Monkey framework as well as the Mercury framework.
-	This allows us to fuzz IPC and UI events at the same time to look for bugs and vulnerabilities.
-*/
-
-#include <iostream>
-#include <cstdlib>
-#include <fstream>
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "config.h"
+#include "utils.h"
 
 using namespace std;
 
-int main(int argc, char* argv[])
-{
-	// cmd line arg parsing
+int main(int argc, char* argv[]) {
 	int aflag = 0;
 	int c;
-	while ((c = getopt (argc, argv, "a:")) != -1)
-	{
+	while ((c = getopt (argc, argv, "a:")) != -1) {
 		switch(c) {	
 		case 'a':
 			aflag = 1;
-			cout << "ADSFASDF";
 			break;
 		default:
 			break;
-			
 		}
 	}
-
-	string AGENTLOC = "../sdk/mercury-2.2.2";	// Location of agent.apk file
-	string EMULOC = "../sdk/sdk/tools";		// Location of emulator
-	string emu_cmd =   "./" + EMULOC + "/emulator @Nexus &";
+	Config CONFIG;
+	//this is hardcoded, BAD BAD BAD
+	string emu_cmd = "./" + CONFIG.get_emuloc() + "/emulator @" 
+						  + CONFIG.get_emuname() + " -scale " 
+						  + f_to_string(CONFIG.get_emuscale()) + " &"; 
 	system(emu_cmd.c_str());
-	string agent_cmd = "adb wait-for-device install " + AGENTLOC + "/agent.apk &";
-	system(agent_cmd.c_str());
-	string 
-	//while(adb shows agent is uninstalled) wait, add timeout
-
-	// emulator64-arm		top
-	// commands used to kill emu process ^^^	
-
-	
+	string agent_cmd = "adb wait-for-device install " + CONFIG.get_agentloc() 
+						  + "/agent.apk &";
+	jpopen(agent_cmd.c_str(), true);
+	string intent_cmd= "adb shell am start -n com.mwr.droidhg.agent/.MainActivity";
+	jpopen(intent_cmd.c_str(), false);
+	jpopen("adb forward tcp:31415 tcp:31415", false);
+	jpopen("adb shell ps | grep com.mwr.droidhg.agent | awk '{print $9}'", true);
 	return 0;
-
 }
-
