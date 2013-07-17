fb7dea4416c7fb024705007754d5ec276ca83466
imbiez <joshrispoli@gmail.com>
2013-07-16 20:32:57 -0500

    popen and removed emu.sh

--- emurun.sh
+++
@@ -1,9 +0,0 @@
-agentloc='../sdk/mercury-2.2.2'
-emuloc='../sdk/sdk/tools'
-emuname='@Nexus'
-emuparams='-scale 0.5 &'
-emu_timer=0
-emu_timeout=25000
-
-eval './'$emuloc'/emulator '$emuname' '$emuparams
-
